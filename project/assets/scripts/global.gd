c230ea8966585358709aadc1e5b201e4776e36e8
David Ma <183250896+MadAvidCoder@users.noreply.github.com>
2024-12-29 15:30:02 +0000

    Type-Cast Global Variables
    Added static type-casting to all global variables.

--- project/assets/scripts/global.gd
+++ project/assets/scripts/global.gd
@@ -1,31 +1,31 @@
 extends Node2D
 
 var http
-var http_response = ""
-var columns = ["id","username","password","has_password","coins","high_score","skins"]
-var user
-var skin = 0
+var http_response: String = ""
+const columns: Array[String] = ["id","username","password","has_password","coins","high_score","skins"]
+var user: String = ""
+var skin: int = 0
 
 func _ready() -> void:
 	http = HTTPRequest.new()
 	add_child(http)
 	http.request_completed.connect(self._http_request_completed)
 
-func new_user(username, password=false):
+func new_user(username: String, password: String = "") -> void:
 	if password:
 		await http_request("https://froggerapi.madavidcoder.hackclub.app/?new=true&username=%s&has_password=true&password=%s" % [username,password],"post")
 	else:
 		await http_request("https://froggerapi.madavidcoder.hackclub.app/?new=true&username=%s&has_password=false" % username,"post")
 
-func edit_user(username, column, value):
+func edit_user(username: String, column: String, value: Variant) -> void:
 	await http_request("https://froggerapi.madavidcoder.hackclub.app/?new=false&username=%s&column=%s&value=%s" % [username,column,value],"post")
 
-func get_all_users():
+func get_all_users() -> Array[String]:
 	var resp = await http_request("https://froggerapi.madavidcoder.hackclub.app/?all=true","get")
 	resp = resp.split(", ")
 	return resp
 
-func get_user_info(username,column=false):
+func get_user_info(username: String, column: String = "") -> Array[String]:
 	var resp = await http_request("https://froggerapi.madavidcoder.hackclub.app/?username=%s" % username,"get")
 	while "'" in resp:
 		resp = resp.replace("'","")
@@ -34,7 +34,7 @@ func get_user_info(username,column=false):
 		return resp[columns.find(column)]
 	return resp
 
-func http_request(url, method):
+func http_request(url: String, method: String) -> String:
 	if method == "get":
 		http.request(url)
 	else:
@@ -42,5 +42,5 @@ func http_request(url, method):
 	await http.request_completed
 	return http_response
 
-func _http_request_completed(result, response_code, headers, body):
+func _http_request_completed(_result, _response_code, _headers, body):
 	http_response = body.get_string_from_utf8()
